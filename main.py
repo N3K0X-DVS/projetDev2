a56a2ae6e7a2fa97553f0799226ff6f10056bdd0
Bommer5 <76489146+Bommer5@users.noreply.github.com>
2024-12-07 14:07:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from interface import FlashcardApp  # Assurez-vous que c'est la classe FlashcardApp
+from DB import creer_base_de_donnees
 
 def run_app():
     app = FlashcardApp()
     app.run()  # Lance l'application
 
 if __name__ == "__main__":
+    creer_base_de_donnees()
     run_app()
